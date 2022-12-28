06e0ae80cc6431909ee88a23202847c62c96e80b
Şermin Atmaca <117165693+serminatmaca@users.noreply.github.com>
2022-12-28 22:15:30 +0300

    OOP.101.Update

---
+++ OOP.101.Draft.New/clsDeCTOR.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._101.Draft
+{
+    internal class clsDeCTOR
+    {
+        ~clsDeCTOR() { }
+    }
+}
+
---
+++ OOP.101.Draft.New/clsStudent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._101.Draft
+{
+    internal class clsStudent
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
+
